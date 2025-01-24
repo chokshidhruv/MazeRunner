0e9c02663fd3da2d55e3cfc64b337a922eea349a
Dhruv <dhruvchokshi005@gmail.com>
2025-01-24 17:10:05 -0500

    Fix: #2: Identify enterance of maze

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -0,0 +1,46 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MazeSolver{
+
+    private static final Logger logger = LogManager.getLogger();
+    // public GenerateMaze generateMaze = new GenerateMaze();
+    public ArrayList<ArrayList<Symbol>> maze = new ArrayList<>();
+    private static String sequenceOfPath = "";
+
+    public void setMaze(ArrayList<ArrayList<Symbol>> maze) {
+        this.maze = maze;
+    }
+
+    public String getSequenceOfPath(){
+        return sequenceOfPath;
+    }
+
+    public void setSequenceOfPath(String singlePath){
+        sequenceOfPath += singlePath;
+    }
+
+    public ArrayList<Integer> findStart(){ //Starts always on the first column
+        ArrayList<Integer> start = new ArrayList<Integer>();
+        for(int i = 0; i < maze.size(); i++){
+            ArrayList<Symbol> row = maze.get(i);
+            if(row.get(0) == Symbol.EMPTY){
+                //Give the starting value as [i, 0] because it will always start on the first column
+                start.add(i);
+                start.add(0);
+                logger.info(String.format("Starting point found at: [%d, 0]", i));
+                return start;
+            }
+        }
+        throw new IllegalArgumentException("No starting point found");
+    }
+
+}
