9a975fc98d971e6f5c152f69b62f7c3aa0a0c0c6
Dhruv <dhruvchokshi005@gmail.com>
2025-02-01 14:59:45 -0500

    Update Main

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,69 +1,33 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
 
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.CommandLine;
-
-import java.util.ArrayList;
-
+import java.io.IOException;
 
 public class Main {
-    
     private static final Logger logger = LogManager.getLogger();
-    private static Options options = new Options();
-    private static CommandLineParser parser = new DefaultParser();
 
     public static void main(String[] args) {
+        logger.info("** Starting Maze Runner **");
 
-        logger.info("** Starting Maze Runner"); //Log the start of the MazeRunner
-        options.addOption("i", true, "Display Maze"); //Add -i flag option to display the maze
+        // Parse command-line arguments
+        ConfigMaze config = new ConfigMaze(null, 0, 0);
+        config.setInputFileName(args);
 
-        GenerateMaze newMaze = new GenerateMaze();
-        MazeSolver mazeSolver = new MazeSolver();
+        try {
+            GenerateMaze mazeGenerator = new GenerateMaze(config.getInputFileName(), config.getMazeHeight(), config.getMazeWidth());
+            MazeSolver solver = new MazeSolver(mazeGenerator.getMaze());
 
-        try{
-            CommandLine cmd = parser.parse(options, args); //Parse the command line arguments
-
-            if(!cmd.hasOption("i")){ //Check if the -i flag is present
-                logger.error("**** No input file specified"); 
-                System.exit(1);
-            }
-
-            String inputFileName = cmd.getOptionValue("i"); //Get the input file name
-            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-
-            logger.info("**** Reading the maze from file " + inputFileName);
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        logger.trace("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        logger.trace("PASS ");
-                    }
-                }
-                logger.trace(System.lineSeparator());
-            }
-            reader.close();
+            logger.info("** Computing Path **");
+            solver.solveMaze();
             
-            ArrayList<ArrayList<Symbol>> maze = newMaze.generateMaze(inputFileName);
-            mazeSolver.setMaze(maze);
-
-            logger.info("**** Computing path");
-
-            mazeSolver.solveMaze();
-        } catch(Exception e){
-            logger.error("/!\\ An error has occured /!\\");
-            logger.error("PATH NOT COMPUTED");
+        } catch (IOException e) {
+            logger.error("Error: " + e.getMessage());
+            System.exit(1);
         }
-        logger.info("** End of MazeRunner");
+
+        logger.info("** End of Maze Runner **");
     }
 }
