8f3e44854678a98f1d2ecb70e461b3dcf7a82e49
Dhruv <dhruvchokshi005@gmail.com>
2025-02-01 19:41:22 -0500

    Fix MazeSolver.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -1,6 +1,6 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public interface MazeSolver{
-    public int[] getNextPosition();
+    public int[] getNextPosition(Cardinal dir);
     public void solveMazePath();
 }
