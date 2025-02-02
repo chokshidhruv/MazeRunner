f116c77d3f417f5bca27bdc38ff14b0039639178
Dhruv <dhruvchokshi005@gmail.com>
2025-02-01 21:28:06 -0500

    Update Main

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -13,18 +13,41 @@ public class Main {
         logger.info("** Starting Maze Runner **");
 
         // Parse command-line arguments
-        ConfigMaze config = new ConfigMaze(null, 0, 0);
+        ConfigMaze config = new ConfigMaze(null, 0, 0, null);
         config.setInputFileName(args);
 
         try {
-            GenerateMaze mazeGenerator = new GenerateMaze(config.getInputFileName(), config.getMazeHeight(), config.getMazeWidth());
-            MazeSolver solver = new MazeSolver(mazeGenerator.getMaze());
-
-            logger.info("** Computing Path **");
-            solver.solveMaze();
+            // Generate the maze using ConfigMaze
+            GenerateMaze newMaze = new GenerateMaze(config.getInputFileName(), config.getMazeHeight(), config.getMazeWidth());
             
-        } catch (IOException e) {
-            logger.error("Error: " + e.getMessage());
+            newMaze.printMaze();
+            
+            MazeLocation loc = new MazeLocation(newMaze.getMaze()); 
+            MazeSolver rightHandAlgorithm = new RightHandAlgorithm(loc);
+
+            logger.info("**** Reading the maze from file: " + config.getInputFileName());
+
+            logger.info("**** Path: " + config.getStringPath());
+
+            boolean isCanonical = true;
+
+            if (config.getStringPath() != null) {
+                if (config.getStringPath() == rightHandAlgorithm.solveMazePath(isCanonical) || config.getStringPath() == rightHandAlgorithm.solveMazePath(!isCanonical)) {
+                    System.out.println("Path is correct");
+                }
+                else {
+                    System.out.println("Path is incorrect");
+                }
+
+            } else {
+                logger.info("**** Computing path");
+                String factorizedPath = rightHandAlgorithm.solveMazePath(false);
+                System.out.println(factorizedPath);
+            }         
+
+        } catch (Exception e) {
+            logger.error("/!\\ An error has occurred /!\\");
+            logger.error("PATH NOT COMPUTED: " + e.getMessage());
             System.exit(1);
         }
 
