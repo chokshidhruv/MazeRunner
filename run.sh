a310283c67e20e5826ac469a11b1ab0da4c214d5
Dhruv <dhruvchokshi005@gmail.com>
2025-02-02 20:34:50 -0500

    Final updates

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Please note, this script is for running the MazeRunner - solely intended for myself to run and test the MazeRunner. 
+# This script is not intended for the user to run the MazeRunner as it has file path hardcoded.
 
 # Install package and run the MazeRunner
 ARGS=$1
 mvn clean package
-# java -jar target/mazerunner.jar -i $ARGS 
 java -jar target/mazerunner.jar -i ./examples/straight.maz.txt
+
 
